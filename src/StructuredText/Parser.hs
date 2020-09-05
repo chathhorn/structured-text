fd8ea81e4ac3cfe2748137a8c3b4059a87f437f0
chathhorn <chathhorn@gmail.com>
2020-09-04 21:32:12 -0400

    Passing tests.

--- src/StructuredText/Parser.hs
+++ src/StructuredText/Parser.hs
@@ -3,12 +3,48 @@ module StructuredText.Parser
     ( parse, parseTop
     ) where
 
+import Control.Applicative ((<|>))
 import Data.Text (Text)
-import Data.Attoparsec.Text (Parser, string, parseOnly)
+import Data.Attoparsec.Text
+      ( Parser, string, parseOnly, many', manyTill, takeTill, isEndOfLine, anyChar)
 import StructuredText.Syntax
-
-parseTop :: Parser STxt
-parseTop = string "Hello World!" *> return (FunBlock "fun")
 
 parse :: Text -> Either String STxt
 parse = parseOnly parseTop
+
+parseTop :: Parser STxt
+parseTop = STxt <$> many' parseGlobal
+
+parseGlobal :: Parser Global
+parseGlobal = parseFunctionBlock
+          <|> parseFunction
+          <|> parseProgram
+          <|> parseTypeDef
+
+parseFunctionBlock :: Parser Global
+parseFunctionBlock = do
+      string "FUNCTION_BLOCK"
+      n <- takeTill isEndOfLine
+      _ <- manyTill anyChar $ string "END_FUNCTION_BLOCK"
+      pure $ FunctionBlock n []
+
+parseFunction :: Parser Global
+parseFunction = do
+      string "FUNCTION"
+      n <- takeTill isEndOfLine
+      _ <- manyTill anyChar $ string "END_FUNCTION"
+      pure $ Function n NoType []
+
+parseProgram :: Parser Global
+parseProgram = do
+      string "PROGRAM"
+      n <- takeTill isEndOfLine
+      _ <- manyTill anyChar $ string "END_PROGRAM"
+      pure $ Program n []
+
+parseTypeDef :: Parser Global
+parseTypeDef = do
+      string "TYPE"
+      n <- takeTill isEndOfLine
+      _ <- manyTill anyChar $ string "END_TYPE"
+      pure $ TypeDef n
--- src/StructuredText/Syntax.hs
+++ src/StructuredText/Syntax.hs
@@ -1,14 +1,36 @@
 {-# LANGUAGE OverloadedStrings, LambdaCase #-}
 module StructuredText.Syntax
       ( STxt (..)
+      , Global (..)
+      , Exp (..)
+      , Type (..)
       ) where
 
 import Data.Text (Text)
 import Prettyprinter ((<+>), Pretty (..))
 
-data STxt = FunBlock Text
+data STxt = STxt [Global]
       deriving Show
 
 instance Pretty STxt where
+      pretty (STxt gs) = pretty gs
+
+data Global = FunctionBlock Text [Exp]
+            | Function Text Type [Exp]
+            | Program Text [Exp]
+            | TypeDef Text
+      deriving Show
+
+instance Pretty Global where
       pretty = \ case
-            FunBlock f -> pretty ("funblock" :: Text) <+> pretty f
+            FunctionBlock f _   -> pretty ("FUNCTION_BLOCK" :: Text) <+> pretty f
+            Function f _ _      -> pretty ("FUNCTION" :: Text) <+> pretty f
+            Program f _         -> pretty ("PROGRAM" :: Text) <+> pretty f
+            TypeDef f           -> pretty ("TYPE" :: Text) <+> pretty f
+
+data Exp = Exp Text
+      deriving Show
+
+data Type = Type Text
+          | NoType
+      deriving Show
