078cf6651de38f822f9c686e9233f49f3a1ea0ec
chathhorn <chathhorn@gmail.com>
2021-03-03 16:58:46 -0500

    Add version and help flags.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,15 @@
 module Main where
 
-import Prelude hiding (readFile, putStrLn)
+import Prelude hiding (readFile, putStr, putStrLn)
 import Control.Monad (when, forM_)
 import Data.Functor (($>))
 import System.Exit (exitFailure, exitSuccess)
 import Data.Text (unpack, pack)
-import Data.Text.IO (readFile, putStrLn)
+import Data.Text.IO (readFile, putStrLn, putStr)
 import System.Environment (getArgs)
 import Text.Megaparsec (parse, errorBundlePretty)
 import System.Console.GetOpt (getOpt, usageInfo, OptDescr (..), ArgOrder (..), ArgDescr (..))
-import System.IO (hPutStr, hPutStrLn, stderr)
+import System.IO (stderr, hPutStrLn)
 
 import StructuredText.Syntax (STxt)
 import StructuredText.Parser (top)
@@ -24,7 +24,7 @@ import qualified Language.Python.Version3.Parser as Py
 import Language.Python.Common.PrettyAST ()
 import Language.Python.Common.PrettyParseError ()
 
-data Flag = NoFlag | FlagP | FlagParsePy | FlagInteractive
+data Flag = NoFlag | FlagP | FlagParsePy | FlagInteractive | FlagVersion | FlagHelp
       deriving (Eq, Show)
 
 options :: [OptDescr Flag]
@@ -32,10 +32,17 @@ options =
       [ Option ['p'] ["parse"]        (NoArg FlagP)           "Print the parsed AST from the ST file arguments."
       , Option []    ["parse-python"] (NoArg FlagParsePy)     "Print the parsed AST from the python file arguments, then exit."
       , Option ['i'] ["interactive"]  (NoArg FlagInteractive) "Enter interactive mode instead of translating argument files."
+      , Option [] ["version"]         (NoArg FlagVersion)     "Print version information."
+      , Option ['h'] ["help"]         (NoArg FlagHelp)        "Print this information."
       ]
 
-exitUsage :: IO ()
-exitUsage = hPutStr stderr (usageInfo "Usage: stxt [OPTION...] <files>" options) >> exitFailure
+printVersion :: IO ()
+printVersion = putStrLn "stxt: a python-to-st (IEC 61131-3 Structured Text) translator, version 1.0."
+
+printUsage :: IO ()
+printUsage = do
+      printVersion
+      putStr $ pack $ (usageInfo "Usage: stxt [OPTION...] <files>" options)
 
 parsePy :: FilePath -> IO (Py.Module ())
 parsePy f = do
@@ -61,12 +68,21 @@ main = do
 
       when (not $ null errs) $ do
             mapM_ (hPutStrLn stderr) errs
-            exitUsage
+            printUsage
+            exitFailure
+
+      when (FlagHelp `elem` flags) $ do
+            printUsage
+            exitSuccess
 
       when (FlagParsePy `elem` flags) $ do
             forM_ filenames $ \ f -> do
                   py <- parsePy f
                   print py
+            exitSuccess
+
+      when (FlagVersion `elem` flags) $ do
+            printVersion
             exitSuccess
 
       forM_ filenames $ \ f -> do
