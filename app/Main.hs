e6b3bd00b51b1faa487c87336b3f24ccf7d6f57f
chathhorn <chathhorn@gmail.com>
2021-03-03 17:46:13 -0500

    Add -o flag and send output to file.

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,13 @@ module Main where
 import Prelude hiding (readFile, putStr, putStrLn)
 import Control.Monad (when, forM_)
 import Data.Functor (($>))
-import System.Exit (exitFailure, exitSuccess)
+import Data.Text.IO (readFile, putStrLn, hPutStrLn)
 import Data.Text (unpack, pack)
-import Data.Text.IO (readFile, putStrLn, putStr)
-import System.Environment (getArgs)
-import Text.Megaparsec (parse, errorBundlePretty)
 import System.Console.GetOpt (getOpt, usageInfo, OptDescr (..), ArgOrder (..), ArgDescr (..))
-import System.IO (stderr, hPutStrLn)
+import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
+import System.FilePath ((-<.>))
+import Text.Megaparsec (parse, errorBundlePretty)
 
 import StructuredText.Syntax (STxt)
 import StructuredText.Parser (top)
@@ -20,20 +20,24 @@ import Shell (shell)
 import qualified Language.Python.Common.AST as Py
 import qualified Language.Python.Common.Pretty as Py
 import qualified Language.Python.Version3.Parser as Py
+import qualified System.IO as SIO
 
 import Language.Python.Common.PrettyAST ()
 import Language.Python.Common.PrettyParseError ()
 
-data Flag = NoFlag | FlagP | FlagParsePy | FlagInteractive | FlagVersion | FlagHelp
+data Flag = NoFlag | FlagParse | FlagParsePy | FlagInteractive | FlagOutput FilePath | FlagVersion | FlagHelp | FlagVerbose
       deriving (Eq, Show)
 
 options :: [OptDescr Flag]
 options =
-      [ Option ['p'] ["parse"]        (NoArg FlagP)           "Print the parsed AST from the ST file arguments."
-      , Option []    ["parse-python"] (NoArg FlagParsePy)     "Print the parsed AST from the python file arguments, then exit."
-      , Option ['i'] ["interactive"]  (NoArg FlagInteractive) "Enter interactive mode instead of translating argument files."
-      , Option [] ["version"]         (NoArg FlagVersion)     "Print version information."
-      , Option ['h'] ["help"]         (NoArg FlagHelp)        "Print this information."
+      [ Option ['p'] ["parse"]        (NoArg FlagParse)                 "Print the parsed AST from the ST file arguments."
+      , Option []    ["parse-python"] (NoArg FlagParsePy)               "Print the parsed AST from the python file arguments, then exit."
+      , Option ['i'] ["interactive"]  (NoArg FlagInteractive)           "Enter interactive mode instead of translating argument files."
+      , Option ['o'] []               (ReqArg FlagOutput $ unpack "filename.py")
+                                                                        "Name for Python output file."
+      , Option ['v'] []               (NoArg FlagVerbose)               "More verbose output."
+      , Option [] ["version"]         (NoArg FlagVersion)               "Print version information."
+      , Option ['h'] ["help"]         (NoArg FlagHelp)                  "Print this information."
       ]
 
 printVersion :: IO ()
@@ -42,7 +46,7 @@ printVersion = putStrLn "stxt: a python-to-st (IEC 61131-3 Structured Text) tran
 printUsage :: IO ()
 printUsage = do
       printVersion
-      putStr $ pack $ (usageInfo "Usage: stxt [OPTION...] <files>" options)
+      SIO.putStr $ usageInfo "Usage: stxt [OPTION...] <files>" options
 
 parsePy :: FilePath -> IO (Py.Module ())
 parsePy f = do
@@ -62,12 +66,18 @@ parseST f = do
                   exitFailure
             Right s -> return s
 
+getOutfile :: [Flag] -> FilePath -> IO String
+getOutfile flags fname = case filter (\ case { FlagOutput {} -> True; _ -> False }) flags of
+      []             -> pure $ fname -<.> "py"
+      [FlagOutput o] -> pure o
+      _              -> hPutStrLn SIO.stderr "Multiple output files specified on the command line!" >> exitFailure
+
 main :: IO ()
 main = do
       (flags, filenames, errs) <-  getOpt Permute options <$> getArgs
 
       when (not $ null errs) $ do
-            mapM_ (hPutStrLn stderr) errs
+            mapM_ (SIO.hPutStrLn SIO.stderr) errs
             printUsage
             exitFailure
 
@@ -86,14 +96,19 @@ main = do
             exitSuccess
 
       forM_ filenames $ \ f -> do
+            when (FlagVerbose `elem` flags) $ putStrLn $ "Parsing: " <> pack f
+
             st <- parseST f
-            when (FlagP `elem` flags) $
+            when (FlagParse `elem` flags) $
                   print st
 
-            case toPython st of
-                  Right py -> putStrLn $ pack (Py.prettyText py)
-                  Left err -> putStrLn $ "Error: " <> err
+            when (FlagVerbose `elem` flags) $ putStrLn $ "Translating: " <> pack f
 
+            case toPython st of
+                  Left err -> hPutStrLn SIO.stderr $ "Error: " <> err
+                  Right py -> do
+                        fout <- getOutfile flags f
+                        SIO.writeFile fout $ Py.prettyText py
 
       when (FlagInteractive `elem` flags) shell
 
