7e9da3afd55cbc15b2bb0a6e0e5b9f7d3cc6ae76
chathhorn <chathhorn@gmail.com>
2021-05-03 17:39:00 -0400

    Map for Buchi transition function.

--- src/StructuredText/Automata.hs
+++ src/StructuredText/Automata.hs
@@ -1,74 +1,100 @@
-module StructuredText.Automata (NFA (..), nfa, union, concat) where
+module StructuredText.Automata
+      ( NFA (..)
+      , nfa
+      , union
+      , concat
+      , intersection
+      ) where
 
 import Prelude hiding (concat)
 import Data.Set (Set, fromList)
+import Data.Map.Strict (Map)
 import Data.Maybe (fromMaybe)
 import qualified Data.Set as S
+import qualified Data.Map.Strict as M
 
-concatMapSet :: (Ord a, Ord b) => (a -> Set b) -> Set a -> Set b
-concatMapSet f s = S.unions (S.map f s)
-
-type Bounds = (Int, Int)
-type States = Set Int
-type BoundedStates = (States, Bounds)
+type Bounds = (Integer, Integer) -- [min, max)
+type State = Integer
+type States = Set State
+-- type BoundedStates = (States, Bounds)
 
 data NFA a = NFA
       { bounds :: Bounds -- First state, last state.
       , inits  :: States
       , final  :: States
-      , delta  :: a -> Int -> States
+      , delta  :: Map State (a -> States)
       }
 
-fize :: (Eq a, Eq b, Ord c) => Set (a, b, c) -> a -> b -> Set c
-fize s a b = S.map thrd (S.filter (\ x -> fst3 x == a && snd3 x == b) s)
-      where fst3 :: (a, b, c) -> a
-            fst3 (a, _, _) = a
+-- concatMapSet :: (Ord a, Ord b) => (a -> Set b) -> Set a -> Set b
+-- concatMapSet f s = S.unions (S.map f s)
 
-            snd3 :: (a, b, c) -> b
-            snd3 (_, b, _) = b
+-- fize :: (Eq a, Eq b, Ord c) => Set (a, b, c) -> a -> b -> Set c
+-- fize s a b = S.map thrd (S.filter (\ x -> fst3 x == a && snd3 x == b) s)
+--       where fst3 :: (a, b, c) -> a
+--             fst3 (a, _, _) = a
+-- 
+--             snd3 :: (a, b, c) -> b
+--             snd3 (_, b, _) = b
+-- 
+--             thrd :: (a, b, c) -> c
+--             thrd (_, _, c) = c
 
-            thrd :: (a, b, c) -> c
-            thrd (_, _, c) = c
+-- run :: Eq a => NFA a -> [a] -> States
+-- run nfa as = run' nfa as (inits nfa)
+--       where run' :: Eq a => NFA a -> [a] -> States -> States
+--             run' nfa (a : as) s = run' nfa as (concatMapSet (delta nfa a) s)
+--             run' _ []         s = s
 
-run :: Eq a => NFA a -> [a] -> States
-run nfa as = run' nfa as (inits nfa)
-      where run' :: Eq a => NFA a -> [a] -> States -> States
-            run' nfa (a : as) s = run' nfa as (concatMapSet (delta nfa a) s)
-            run' _ []         s = s
+-- A perhaps more convenient NFA constructor:
+-- init states,
+-- final states,
+-- other states,
+-- transition function (delta).
+nfa :: [State] -> [State] -> [State] -> (State -> a -> [State]) -> NFA a
+nfa ins fins others f = NFA (lower, upper) ins' fins' d
+      where lower :: Integer
+            lower = fromMaybe 0 $ S.lookupMin allStates
 
-nfa :: [Int] -> [Int] -> (a -> Int -> [Int]) -> NFA a
-nfa i f d = NFA (lower, upper) ins fins (\ a b -> fromList (d a b))
-      where lower :: Int
-            lower = fromMaybe minBound $ S.lookupMin ins
+            upper :: Integer
+            upper = fromMaybe 0 $ S.lookupMax allStates
 
-            upper :: Int
-            upper = fromMaybe maxBound $ S.lookupMax fins
+            ins' :: States
+            ins' = fromList ins
 
-            ins :: States
-            ins = fromList i
+            fins' :: States
+            fins' = fromList fins
 
-            fins :: States
-            fins = fromList f
+            others' :: States
+            others' = fromList others
 
-shift :: Bounds -> Bounds -> Int
+            allStates :: States
+            allStates = ins' `S.union` fins' `S.union` others'
+
+            -- d :: Map State (a -> States)
+            d = M.fromSet (\ s -> fromList . (f s)) allStates
+
+shift :: Bounds -> Bounds -> Integer
 shift aBnds bBnds | fst bBnds >= snd aBnds = 0
 shift aBnds bBnds = snd aBnds - fst bBnds
 
 bnds :: Bounds -> Bounds -> Bounds
 bnds aBnds bBnds = (fst aBnds, (shift aBnds bBnds) + snd bBnds)
 
-djUnion :: Int -> States -> States -> States
+djUnion :: Integer -> States -> States -> States
 djUnion sh a = S.union a . S.map (+ sh)
 
-deltaUnion :: Int -> (a -> Int -> States) -> (a -> Int -> States) -> a -> Int -> States
-deltaUnion sh a b w s = S.union (a w s) (S.map (+ sh) $ b w (s - sh))
+deltaUnion :: Integer
+           -> Map State (a -> States)
+           -> Map State (a -> States)
+           -> Map State (a -> States)
+deltaUnion sh a = M.union a . M.mapKeys (+ sh) . M.map (S.map (+ sh) .)
 
 union :: NFA a -> NFA a -> NFA a
 union a b = NFA (bnds (bounds a) (bounds b))
       (djUnion sh (inits a) (inits b))
       (djUnion sh (final a) (final b))
       (deltaUnion sh (delta a) (delta b))
-      where sh :: Int
+      where sh :: Integer
             sh = shift (bounds a) (bounds b)
 
 intersection :: NFA a -> NFA a -> NFA a
@@ -83,8 +109,15 @@ concat a b = NFA (bnds (bounds a) (bounds b))
             i' = if S.disjoint (inits a) (final a)
                  then inits a else S.union (inits a) (S.map (+ sh) $ inits b)
 
-            conc f w s | S.disjoint (f w s) (final a) = f w s
-            conc f w s                                = S.union (f w s) (S.map (+ sh) $ inits b)
+            conc :: Map State (a -> States) -> Map State (a -> States)
+            conc = M.map $ \ f w -> if S.disjoint (f w) (final a) then (f w)
+                        else S.union (f w) (S.map (+ sh) $ inits b)
+            -- conc = M.map (\ s -> if S.disjoint s (final a) then s
+            --    else S.union s (S.map (+ sh) $ inits b))
+            -- conc f w s | S.disjoint (f w s) (final a)
+            --       = f w s
+            -- conc f w s
+            --       = S.union (f w s) (S.map (+ sh) $ inits b)
 
-            sh :: Int
+            sh :: Integer
             sh = shift (bounds a) (bounds b)
--- src/StructuredText/ToBuchi.hs
+++ src/StructuredText/ToBuchi.hs
@@ -4,8 +4,6 @@ import StructuredText.Automata (NFA, nfa)
 import qualified StructuredText.Automata as A
 import StructuredText.LTL (NormLTL (..))
 
-import Data.Set (fromList)
-
 -- TermN    a
 -- AndN     (NormLTL a) (NormLTL a)
 -- OrN      (NormLTL a) (NormLTL a)
@@ -15,13 +13,13 @@ import Data.Set (fromList)
 
 toBuchi :: (a -> w -> Bool) -> NormLTL a -> NFA w
 toBuchi trueAt = \ case
-      TermN a      -> nfa [0] [2] (f a)
-            where f a w 0 | a `trueAt` w = [2]
-                  f _ _ 2                = [2]
-                  f _ _ _                = [1]
+      TermN a      -> nfa [0] [2] [1] f
+            where f 0 w | a `trueAt` w = [2]
+                  f 2 _              = [2]
+                  f _ _              = [1]
       AndN a b     -> A.intersection (toBuchi trueAt a) (toBuchi trueAt b)
       OrN a b      -> A.union (toBuchi trueAt a) (toBuchi trueAt b)
       UntilN a b   -> undefined
       ReleaseN a b -> undefined
-      NextN a      -> A.concat (nfa [0] [1] f) (toBuchi trueAt a)
+      NextN a      -> A.concat (nfa [0] [1] [] f) (toBuchi trueAt a)
             where f _ _ = [1]
